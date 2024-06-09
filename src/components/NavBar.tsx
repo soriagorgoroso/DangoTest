31feabfd7b26f7b444cb1f02acd1fc8d888a5261
Sebastian Soria <soriagorgoroso@gmail.com>
2024-06-09 20:03:38 -0300

    Inicializacion del proyecto se configuro tailwind

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="bg-discountBg py-4">
+      <div className="container mx-auto">
+        <p className="text-center text-white">30% Off All Orders Until 4/27</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        discountBg: "#7AA65A",
+      },
+    },
+  },
+  plugins: [],
+};

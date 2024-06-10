cf6619710e7e98f890397a53c11a8fc69ebed518
Sebastian Soria <soriagorgoroso@gmail.com>
2024-06-10 13:20:50 -0300

    Cambio src img

--- src/helpers/mocks/products.ts
+++ src/helpers/mocks/products.ts
@@ -1,35 +1,35 @@
 export const products = [
   {
-    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/public/img/model2.png?raw=true`,
+    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/src/assets/model2.png?raw=true`,
     price: 176.98,
   },
   {
-    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/public/img/model3.png?raw=true`,
+    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/src/assets/model3.png?raw=true`,
     price: 126.98,
   },
   {
-    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/public/img/model4.png?raw=true`,
+    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/src/assets/model4.png?raw=true`,
     price: 136.98,
   },
   {
-    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/public/img/model5.png?raw=true`,
+    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/src/assets/model5.png?raw=true`,
     price: 146.98,
   },
   {
-    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/public/img/model2.png?raw=true`,
+    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/src/assets/model2.png?raw=true`,
     price: 156.98,
   },
   {
-    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/public/img/model3.png?raw=true`,
+    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/src/assets/model3.png?raw=true`,
     price: 166.98,
   },
 
   {
-    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/public/img/model4.png?raw=true`,
+    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/src/assets/model4.png?raw=true`,
     price: 176.98,
   },
   {
-    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/public/img/model3.png?raw=true`,
+    imgUrl: `https://github.com/soriagorgoroso/DangoTest/blob/main/src/assets/model3.png?raw=true`,
     price: 166.98,
   },
 ];
